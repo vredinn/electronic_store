47ed3e5da9593e777f628dd8f937ff9c1a437e33
Victor Dmitriev <fox3un@gmail.com>
2025-06-18 13:24:50 +0500

    fix create order

--- order/crud.py
+++ order/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from . import schemas
 import models
+from product import crud as prod_crud
 
 
 def get_order(db: Session, order_id: int):
@@ -15,23 +16,26 @@ def get_orders(db: Session, user_id: int = None, skip: int = 0, limit: int = 100
 
 
 def create_order(db: Session, order: schemas.OrderCreate, user_id: int):
-    db_order = models.Order(user_id=user_id, status=order.status, amount=0)
+    db_order = models.Order(
+        user_id=user_id, status=models.OrderStatus.PENDING, amount=0
+    )
     db.add(db_order)
     db.commit()
     db.refresh(db_order)
 
-    total = 0
+    amount = 0
     for item in order.items:
+        price_at_order = prod_crud.get_product(db, item.product_id).price
         db_item = models.OrderItem(
             order_id=db_order.id,
             product_id=item.product_id,
             quantity=item.quantity,
-            price_at_order=item.price_at_order,
+            price_at_order=price_at_order,
         )
-        total += item.price_at_order * item.quantity
+        amount += price_at_order * item.quantity
         db.add(db_item)
 
-    db_order.amount = total
+    db_order.amount = amount
     db.commit()
     db.refresh(db_order)
     return db_order
--- order/schemas.py
+++ order/schemas.py
@@ -7,13 +7,16 @@ from models import OrderStatus
 
 class OrderItemBase(BaseModel):
     product_id: int
-    product_name: str
     quantity: int = Field(..., gt=0)
+
+
+class OrderItem(OrderItemBase):
+    product_name: str
     price_at_order: Decimal = Field(..., gt=0, decimal_places=2)
 
 
 class OrderBase(BaseModel):
-    status: OrderStatus = Field(default=OrderStatus.PENDING)
+    pass
 
 
 class OrderCreate(OrderBase):
@@ -26,12 +29,13 @@ class OrderUpdate(BaseModel):
 
 class Order(OrderBase):
     id: int
+    status: OrderStatus = Field(default=OrderStatus.PENDING)
     user_id: int
     user_name: str
     amount: Decimal
     created_at: datetime
     updated_at: Optional[datetime]
-    items: List[OrderItemBase]
+    items: List[OrderItem]
 
     class Config:
         from_attributes = True
